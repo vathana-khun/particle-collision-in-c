4fb46926993e7c38db0789bd69438bc319efb5a4
vking0 <khunvathana2@gmail.com>
2024-06-19 17:37:42 +1000

    update most of the function as a result of delta time and just do some refactoring

--- constant.h
+++ constant.h
@@ -4,4 +4,4 @@
 #define FPS 60
 #define FRAME_TARGET_TIME (1000/FPS)
 
-#define AMOUNT_PARTICLE 100
+#define AMOUNT_PARTICLE 1
--- particlecollision.c
+++ particlecollision.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <time.h>
 #include <stdbool.h>
+
 #include "constant.h"
  
 struct particle{
@@ -11,11 +12,10 @@ struct particle{
     float radius;
 };
 
-
 SDL_Window *window = NULL; 
 SDL_Renderer *renderer = NULL;
 bool game_is_running = false;
-int last_frame_time = 0;
+float last_frame_time = 0;
 struct particle particles[AMOUNT_PARTICLE];
 float gravity;
 float collision_damping;
@@ -25,9 +25,11 @@ int numParticles = sizeof(particles) / sizeof(particles[0]);
 void circle(SDL_Renderer*renderer, int x_center, int y_center, int r);
 void create_particle(struct particle p[], int r, int numParticles);
 void render_particle(SDL_Renderer *render,struct particle p[],  int numParticles);
-void update_particle(struct particle p[],  int numParticles);
-void collision_detection(struct particle p[], int numParticles);
-void solve_collision(struct particle p[], int numParticles);
+void update_particle(struct particle p[],  int numParticles,float dt);
+void collision_detection(struct particle *P, int numParticles, float dt);
+void solve_collision(struct particle *p1, struct particle *p2, float dt);
+bool collide(struct particle *p1, struct particle *p2);
+void boundary_collision(struct particle *p, float dt);
 
 
 bool intialize_window(void){
@@ -51,8 +53,8 @@ bool intialize_window(void){
 }
 
 void setup(){
-    gravity = 0.5f;
-    collision_damping = 0.9f;
+    gravity = 4*60;
+    collision_damping = 0.7f;
     create_particle(particles, 10, numParticles);
     // particles[0].position[0] = WIDTH/4;
     // particles[0].position[1] = HEIGHT/2;
@@ -84,17 +86,15 @@ void process_input(){
 }
 void update(){
     int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks()- last_frame_time);
-
-    if (time_to_wait >0 && time_to_wait <= FRAME_TARGET_TIME) {
+    if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
         SDL_Delay(time_to_wait);
     }
-    last_frame_time = SDL_GetTicks();
+    float dt = (SDL_GetTicks() - last_frame_time) /1000.0f;
+    last_frame_time  = SDL_GetTicks();
 
-    update_particle(particles, numParticles);
-    collision_detection(particles, numParticles);
-    solve_collision(particles, numParticles);
-    
-
+    // printf("Delta Time is: %f\n",dt);
+    update_particle(particles, numParticles,dt);
+    collision_detection(particles, numParticles, dt);
 }
 void render(){
     SDL_SetRenderDrawColor(renderer,255,255,255,255);
@@ -117,7 +117,6 @@ int main ( int argc, char*argv[]){
     game_is_running = intialize_window();
 
     srand(time(0));
-
     setup();
 
     while(game_is_running){
@@ -126,7 +125,6 @@ int main ( int argc, char*argv[]){
         render();  
     }
     clean();
-
     return 0;
 }
 
@@ -157,157 +155,151 @@ void circle(SDL_Renderer*renderer, int x_center, int y_center, int r){
 }
 
 void create_particle(struct particle p[], int r, int numParticles){
-    
-
     for(int i = 0; i < numParticles; i++){
         //First Loop Set Radius
         p[i].radius = r;
         //Loop Twice for both position and velocity
-        
         p[i].position[0] = rand() % ( (WIDTH - 20) - 20 + 1 ) + 20;
         p[i].position[1] = rand() % ( (HEIGHT - 20) - 20 + 1 ) + 20;
 
-        p[i].velocity[0] = rand() % (5 - 1 + 1) + 1;
-        p[i].velocity[1] = rand() % (5- 1 + 1) + 1;
+        // p[i].velocity[0] = (rand() % (3 - 1 + 1) + 1) * 60;
+        // p[i].velocity[1] = (rand() % (3 - 1 + 1) + 1 )* 60;
+
+        p[i].velocity[0] = 0;
+        p[i].velocity[1] = 100;
+
+        // printf("creating velocity of particle:%d (%f , %f)\n", i , p[i].velocity[0],p[i].velocity[1]);
     }
 }
 
-void update_particle(struct particle p[], int numParticles){
+void update_particle(struct particle p[], int numParticles,float dt){
     for(int i = 0; i < numParticles; i++){
-        float *x = &(p[i].position[0]);
-        float *y = &(p[i].position[1]);
-        float *xspeed = &(p[i].velocity[0]);
-        float *yspeed = &(p[i].velocity[1]);
+        // printf(" before update velocity of particle:%d (%f , %f) dt is %f\n", i , p[i].velocity[0],p[i].velocity[1], dt);
+        p[i].position[0] += p[i].velocity[0] * dt ;
+        p[i].position[1] += p[i].velocity[1] * dt;
 
-        *yspeed += gravity;
+        p[i].velocity[1] += gravity * dt;
+        
+        boundary_collision(&p[i], dt);
 
-        *x += *xspeed;
-        *y += *yspeed;
+        // printf(" after update velocity of particle:%d (%f , %f) dt is %f\n", i , p[i].velocity[0],p[i].velocity[1],dt);
     }
 }
 
-void collision_detection(struct particle p[], int numParticles){
-    
-for (int i = 0; i < numParticles; i++) {
-    int x = p[i].position[0];
-    int y = p[i].position[1];
-    int r = p[i].radius;
-
-    // Check boundaries and handle collision
-    if (x > WIDTH - r) {
-        // Adjust position
-        p[i].position[0] = 2 * (WIDTH - r) - x;
-        // Reverse velocity
-        p[i].velocity[0] *= -1;
-    } else if (x < r) {
-        // Adjust position
-        p[i].position[0] = 2 * r - x;
-        // Reverse velocity
-        p[i].velocity[0] *= -1;
-    }
-
-    if (y > HEIGHT - r) {
-        // Adjust position
-        p[i].position[1] = 2 * (HEIGHT - r) - y;
-        // Reverse velocity
-        p[i].velocity[1] *= -1;
-    } else if (y < r) {
-        // Adjust position
-        p[i].position[1] = 2 * r - y;
-        // Reverse velocity
-        p[i].velocity[1] *= -1;
-    }
-}
-}
-
-void solve_collision(struct particle p[], int numParticles){
-     for(int i = 0; i < numParticles; i++){
-        for(int j = i + 1 ; j< numParticles; j++){
-            double d = sqrt((pow (p[i].position[0] - p[j].position[0],2)) + (pow (p[i].position[1] - p[j].position[1],2)));
-            double min_dist = p[i].radius + p[j].radius; 
-            float m1 = p[i].radius;
-            float m2 = p[i].radius;       
-            if(d < min_dist){  
-
-                float x1 = p[i].position[0];
-                float y1 = p[i].position[1];
-                float v1_x = p[i].velocity[0];
-                float v1_y = p[i].velocity[1];
-
-                float x2 = p[j].position[0];
-                float y2 = p[j].position[1];
-                float v2_x = p[j].velocity[0];
-                float v2_y = p[j].velocity[1];
-
-                // calculated unit normal vector
-                float un_x = (x2 - x1) / d;
-                float un_y = (y2 - y1) / d;
-
-                //calculated unit tagnent vector
-                float ut_x = un_y * -1;
-                float ut_y =  un_x;
-
-                //Correct the first 
-                float correction = (d - min_dist);
-                float correction_x = un_x * correction / 2;
-                float correction_y = un_y * correction / 2; 
-
-                x1 += correction_x;
-                y1 += correction_y;
-
-                x2 -= correction_x;
-                y2 -= correction_y;
-
-                // Calculated the Scalar Part.
-                float v1_n = (v1_x * un_x) + (v1_y * un_y);  
-                float v2_n = (v2_x * un_x) + (v2_y * un_y); 
-                float v1_t = (v1_x * ut_x) + (v1_y * ut_y);
-                float v2_t = (v2_x * ut_x) + (v2_y * ut_y);
-
-                float vf1_t = v1_t;
-                float vf2_t = v2_t;
-
-                float vf1_n = (( v1_n * (m1 - m2) + 2 * m2 * v2_n) / (m1+m2));
-                float vf2_n = (( v2_n * (m2 - m1) + 2 * m1 * v1_n) / (m1+m2));
-
-                //Converting to Vector
-                float vf1_n_x = vf1_n * un_x;
-                float vf1_n_y = vf1_n * un_y;
-                float vf2_n_x = vf2_n * un_x;
-                float vf2_n_y = vf2_n * un_y;    
-
-                float vf1_t_x = vf1_t * ut_x;
-                float vf1_t_y = vf1_t * ut_y;
-                float vf2_t_x = vf2_t * ut_x;
-                float vf2_t_y = vf2_t *ut_y;
-
-                float vf1_x = vf1_n_x + vf1_t_x;
-                float vf1_y = vf1_n_y + vf1_t_y;
-                float vf2_x = vf2_n_x + vf2_t_x;
-                float vf2_y = vf2_n_y + vf2_t_y;
-
-
-                p[i].velocity[0] = vf1_x;
-                p[i].velocity[1] = vf1_y;
-                p[j].velocity[0] = vf2_x;
-                p[j].velocity[1] = vf2_y;
-
-                p[i].velocity[0] *= collision_damping;
-                p[i].velocity[1] *= collision_damping;
-
-                p[i].position[0] = x1;
-                p[i].position[1] = y1;
-                p[j].position[0] = x2;
-                p[j].position[1] = y2;
-
+void collision_detection(struct particle p[], int numParticles, float dt){
+    for (int i = 0; i < numParticles; i++) {
+       for (int j = i + 1 ; j<numParticles; j++){
+            if (collide(&p[i] , &p[j])){
+                solve_collision(&p[i] , &p[j],dt);
             }
-
-            }   
+       }
+    }
+}
+void boundary_collision(struct particle *p, float dt){
+    float x = p -> position[0];
+    float y = p -> position[1];
+    float r = p -> radius;
+        // Check boundaries and handle collision
+        // printf("Before boundary collision - Position (%f, %f), Velocity (%f, %f)\n", x, y, p->velocity[0], p->velocity[1]);
+        if (x > WIDTH - r) {
+            p->position[0] = 2 * (WIDTH - r) - x;
+            p->velocity[0] *= -1 * collision_damping ;
+        } else if (x < r) {
+            p->position[0] = 2 * r - x;
+            p->velocity[0] *= -1 * collision_damping ;
         }
-     }
+        if (y > HEIGHT - r) {
+            p->position[1] = 2 * (HEIGHT - r) - y;
+            p->velocity[1] *= -1 * collision_damping ;
+        } else if (y < r) {
+            p->position[1] = 2 * r - y;
+            p->velocity[1] *= -1 * collision_damping ;
+        }
+        //  printf("After boundary collision - Position (%f, %f), Velocity (%f, %f)\n", p->position[0], p->position[1], p->velocity[0], p->velocity[1]);
+}
+bool collide(struct particle *p1, struct particle *p2){
+        double d = sqrt((pow (p1->position[0] - p2->position[0],2)) + (pow (p1->position[1] - p2->position[1],2)));
+        double min_dist = p1->radius + p2->radius; 
+        if(d < min_dist){ 
+            return true;
+        }
+    return false;
+}
+void solve_collision(struct particle *p1, struct particle *p2, float dt){
+    float x1 = p1->position[0];
+    float y1 = p1->position[1];
+    float v1_x = p1->velocity[0];
+    float v1_y = p1->velocity[1];
 
+    double d = sqrt((pow (p1->position[0] - p2->position[0],2)) + (pow (p1->position[1] - p2->position[1],2)));
+    double min_dist = p1->radius + p2->radius;
+    float m1 = p1->radius*0.1;    
+    float m2 = p2->radius*0.1; 
 
+    float x2 = p2->position[0];
+    float y2 = p2->position[1];
+    float v2_x = p2->velocity[0];    float v2_y = p2->velocity[1];
 
+    // calculated unit normal vector
+    float un_x = (x2 - x1) / d;
+    float un_y = (y2 - y1) / d;
+
+    //calculated unit tagnent vector
+    float ut_x = un_y * -1;
+    float ut_y =  un_x;
+
+    //Correct the first 
+    float correction = (d - min_dist);
+    float correction_x = un_x * correction / 2;
+    float correction_y = un_y * correction / 2; 
+
+    x1 += correction_x;
+    y1 += correction_y;
+
+    x2 -= correction_x;
+    y2 -= correction_y;
+
+    // Calculated the Scalar Part.
+    float v1_n = (v1_x * un_x) + (v1_y * un_y);  
+    float v2_n = (v2_x * un_x) + (v2_y * un_y); 
+    float v1_t = (v1_x * ut_x) + (v1_y * ut_y);
+    float v2_t = (v2_x * ut_x) + (v2_y * ut_y);
+
+    float vf1_t = v1_t;
+    float vf2_t = v2_t;
+
+    float vf1_n = (( v1_n * (m1 - m2) + 2 * m2 * v2_n) / (m1+m2));
+    float vf2_n = (( v2_n * (m2 - m1) + 2 * m1 * v1_n) / (m1+m2));
+
+    //Converting to Vector
+    float vf1_n_x = vf1_n * un_x;
+    float vf1_n_y = vf1_n * un_y;
+    float vf2_n_x = vf2_n * un_x;
+    float vf2_n_y = vf2_n * un_y;    
+
+    float vf1_t_x = vf1_t * ut_x;
+    float vf1_t_y = vf1_t * ut_y;
+    float vf2_t_x = vf2_t * ut_x;
+    float vf2_t_y = vf2_t *ut_y;
+
+    float vf1_x = vf1_n_x + vf1_t_x;
+    float vf1_y = vf1_n_y + vf1_t_y;
+    float vf2_x = vf2_n_x + vf2_t_x;
+    float vf2_y = vf2_n_y + vf2_t_y;
+
+    p1->velocity[0] = vf1_x;
+    p1->velocity[1] = vf1_y;
+    p2->velocity[0] = vf2_x;
+    p2->velocity[1] = vf2_y;
+
+    p1->velocity[0] *= collision_damping ;
+    p1->velocity[1] *= collision_damping ;
+    
+    p1->position[0] = x1;
+    p1->position[1] = y1;
+    p2->position[0] = x2;
+    p2->position[1] = y2;
+}
 void render_particle(SDL_Renderer *render,struct particle p[], int numParticles){
    
     for(int i = 0; i < numParticles; i++){
